9c235da0eca02f4291ae96d5f967e37082b751d6
bodwood <bodiewood@hotmail.com>
2023-02-27 10:03:21 -0800

    create form and return count

--- Lorem-Ipsum/src/App.js
+++ Lorem-Ipsum/src/App.js
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(data);
   
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const numData = formData.get('numberInput')
+    console.log(numData);
+    setCount(numData)
+    
+    
+  }
   
   return (
-  <h2>lorem ipsum project setup</h2>
+  <div>
+    <h1>Tired of boring lorem ipsum?</h1>
+    <form action="submit" onSubmit={handleSubmit}>
+      <label htmlFor="number">Paragraphs</label>
+      <input type="number" name="numberInput" id="number" />
+      <button type='submit' name='numberInput' className='btn' >Generate</button>
+    </form>
+    <p>
+      {value.map((e) => {
+        const {text} = e;
+        return text
+
+      })}
+    </p>
+  </div>
     )
 }
 
