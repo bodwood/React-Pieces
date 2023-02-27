6ddbbb1fc68d7e0b2323e0e01ee27500ee753220
bodwood <bodiewood@hotmail.com>
2023-02-27 10:38:36 -0800

    add function to display paragraphs depending on state value of data

--- Lorem-Ipsum/src/App.js
+++ Lorem-Ipsum/src/App.js
@@ -2,35 +2,47 @@ import React, { useState } from 'react';
 import data from './data';
 function App() {
   const [count, setCount] = useState(0);
-  const [value, setValue] = useState(data);
-  
+  const [value, setValue] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const numData = formData.get('numberInput')
-    console.log(numData);
-    setCount(numData)
-    
-    
-  }
-  
-  return (
-  <div>
-    <h1>Tired of boring lorem ipsum?</h1>
-    <form action="submit" onSubmit={handleSubmit}>
-      <label htmlFor="number">Paragraphs</label>
-      <input type="number" name="numberInput" id="number" />
-      <button type='submit' name='numberInput' className='btn' >Generate</button>
-    </form>
-    <p>
-      {value.map((e) => {
-        const {text} = e;
-        return text
+    let amount = 0;
+    if(count <= 0)
+    {
+      amount = 1
+    }
+    else if(count > 8)
+    {
+      amount = 8;
+    }
+    else{
+      amount = parseInt(count)
+    }
+    setValue(data.slice(0, amount));
+    console.log(amount)
+  };
 
-      })}
-    </p>
-  </div>
-    )
+  return (
+    <section className='section-center'>
+      <h1>Tired of boring lorem ipsum?</h1>
+      <form action='submit' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>Paragraphs</label>
+        <input type='number' name='numberInput' id='number' value={count} onChange={(e) => setCount(e.target.value)} />
+        <button type='submit' className='btn'>
+          Generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {value.map((item, index) => {
+          return (
+            <div key='index'>
+              <p>{item}</p>
+            </div>
+          );
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
